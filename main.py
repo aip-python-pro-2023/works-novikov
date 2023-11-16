c6940383d29eadafaa2c835647ce4309bbea4d36
IvanNovikov0 <novikov.i1305@gmail.com>
2023-11-17 00:12:33 +0300

    Merge remote-tracking branch 'origin/main' into main

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import telebot
 from telebot import types
 load_dotenv()
 
-##TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
-TELEGRAM_TOKEN = '6721522547:AAF_x3ZT6hDC1zRzNd5KjSpxZYKFB-ak_-s'
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+
 bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None)
 
 
